fdd61a59610749702ad14d48f6866a27a55e8e4e
Nidhi <nidhichandu@gmail.com>
2019-06-23 12:05:51 -0500

    "Completed Module 5"

---
+++ src/QuizEnd.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class QuizEnd extends Component {
+  render() {
+    return (
+      <div>
+        <p>Thanks for Playing!</p>
+        <a href="www.react.com">Reset Quiz</a>
+      </div>
+    );
+  }
+}
+
+export default QuizEnd;
